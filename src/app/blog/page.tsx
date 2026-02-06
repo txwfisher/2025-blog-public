55be99db8e29730b38c69b430fa72822f84144bd
fisher <91155330+txwfisher@users.noreply.github.com>
2026-02-06 22:44:51 +0800

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -382,7 +382,7 @@ export default function BlogPage() {
 							animate={{ opacity: 1, scale: 1 }}
 							whileHover={{ scale: 1.05 }}
 							whileTap={{ scale: 0.95 }}
-							href='https://tucong.vip/'
+							href='https://juejin.cn/user/3194529563289911'
 							target='_blank'
 							className='card text-secondary static inline-flex items-center gap-2 rounded-xl px-4 py-2 text-xs'>
 							<JuejinSVG className='h-4 w-4' />
